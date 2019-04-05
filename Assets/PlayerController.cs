44e554c66fd37861dd3e36147b9e08f7bc3c0f9f
Gruntun <49292426+Gruntun@users.noreply.github.com>
2019-04-04 19:23:31 -0700

    worked on I AM SPEED

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,37 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
-
-    private Vector2 moveVelocity;
-
-    private Rigidbody2D rb;
-
-    private Vector3 change;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        Vector2 moveinput = new Vector2( Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-        moveVelocity = moveinput.normalized * speed;
-
-        
-
-        
-    }
-
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
-
-
-
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal")
+            , 0.0f, 0.0f);
     }
 
 
