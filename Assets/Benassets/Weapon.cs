b087155de512f9dad5586f3196b9cdfd442cc62b
Gruntun <49292426+Gruntun@users.noreply.github.com>
2019-04-04 17:25:53 -0700

    la

---
+++ Assets/Benassets/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public Transform center;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        transform.LookAt(center);
+
+    }
+
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
