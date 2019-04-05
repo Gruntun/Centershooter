6fa6d2c40786433b1ae26aca0f8bedce4b1f015d
StarrKiss <gregorystarr00@gmail.com>
2019-04-05 11:32:40 -0500

    Add movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
+    public float slowdownspeed;
+
+    public Rigidbody2D rb;
 
     void Update()
     {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal")
-            , 0.0f, 0.0f);
+       if (Input.GetKey (KeyCode.A)) {
+rb.AddForce (-Vector2.right * speed * Time.deltaTime);
+}
+if (Input.GetKey (KeyCode.D)) {
+rb.AddForce (Vector2.right * speed * Time.deltaTime);
+}
+if (Input.GetKey (KeyCode.S)) {
+rb.AddForce (-Vector2.up * speed * Time.deltaTime);
+}
+if (Input.GetKey (KeyCode.W)) {
+rb.AddForce (Vector2.up * speed * Time.deltaTime);
+}
+
+slowdown();
+    }
+
+    void slowdown(){
+
+        rb.velocity = rb.velocity * new Vector2(slowdownspeed, slowdownspeed);
+
     }
 
 
