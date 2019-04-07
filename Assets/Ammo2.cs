857cacd02353d939c0d1c748152da2a828805b99
StarrKiss <gregorystarr00@gmail.com>
2019-04-07 09:24:16 -0500

    Added Ammo bars

---
+++ Assets/Ammo2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ammo2 : MonoBehaviour
+{
+
+     private Slider ammobar;
+
+    public Weapon wep;
+
+     private void Awake() {
+      ammobar = GetComponent<Slider>(); 
+   }
+
+    // Update is called once per frame
+    void Update()
+    {
+                ammobar.value = wep.ammo;
+
+    }
+}
