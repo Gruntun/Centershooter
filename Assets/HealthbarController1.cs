260e093c1553ca9a0b187e4c565b0e1976121b9d
StarrKiss <gregorystarr00@gmail.com>
2019-04-07 09:19:10 -0500

    Added healthbar

---
+++ Assets/HealthbarController1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthbarController1 : MonoBehaviour
+{
+
+    private Slider healthBar;
+
+    public PlayerHealth ph;
+
+   
+    // Start is called before the first frame update
+
+
+     private void Awake() {
+
+        healthBar = GetComponent<Slider>();
+        
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+         healthBar.value = ph.Health;
+    }
+
+    
+}
